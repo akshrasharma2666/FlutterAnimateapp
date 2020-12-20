491cbd5f0dd57dc6f20804aa74e0d9d27383e347
thetechgurl2666 <52304108+akshrasharma2666@users.noreply.github.com>
2020-12-20 16:31:55 +0530

    initially uploaded

---
+++ animationapp/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:animationapp/animation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        
+        body: MyAnimationApp(),
+      ),
+    ),
+  );
+}
